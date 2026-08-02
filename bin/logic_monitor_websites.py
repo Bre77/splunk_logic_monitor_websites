6bb5e39142214b7188a3aec5a73bf69158efe31c
Brett Adams <hi@ba.au>
2026-08-03 02:12:50 +1000

    Migrate to splunk_input_runtime, drop splunk-sdk dependency
    Move off splunk-sdk onto the fleet's in-house splunk_input_runtime, the
    one-secret-wave migration for this app following the pilot pattern in
    TA_thinkst_canary_audit.

--- bin/logic_monitor_websites.py
+++ bin/logic_monitor_websites.py
@@ -5,7 +5,7 @@ import time
 import requests
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
-from splunklib.modularinput import Script, Scheme, Argument, EventWriter, Event
+from splunk_input_runtime.modularinput import Argument, Event, EventWriter, Scheme, Script
 
 
 class Input(Script):
@@ -49,7 +49,6 @@ class Input(Script):
         return scheme
 
     def stream_events(self, inputs, ew):
-        self.service.namespace["app"] = self.APP
         # Get Variables
         input_name, input_items = inputs.inputs.popitem()
         kind, name = input_name.split("://")
@@ -61,31 +60,14 @@ class Input(Script):
         )
 
         # Password Encryption
-        updates = {}
-
-        for item in ["token"]:
-            stored_password = [
-                x
-                for x in self.service.storage_passwords
-                if x.username == item and x.realm == name
-            ]
-            if input_items[item] == self.MASK:
-                if len(stored_password) != 1:
-                    ew.log(
-                        EventWriter.ERROR,
-                        f"Encrypted {item} was not found for {input_name}, reconfigure its value.",
-                    )
-                    return
-                input_items[item] = stored_password[0].content.clear_password
-            else:
-                if stored_password:
-                    ew.log(EventWriter.DEBUG, "Removing Current password")
-                    self.service.storage_passwords.delete(username=item, realm=name)
-                ew.log(EventWriter.DEBUG, "Storing password and updating Input")
-                self.service.storage_passwords.create(input_items[item], item, name)
-                updates[item] = self.MASK
-        if updates:
-            self.service.inputs.__getitem__((name, kind)).update(**updates)
+        secrets = self.context.credentials.protect_input_fields(
+            kind=kind,
+            stanza=name,
+            values=input_items,
+            fields=("token",),
+            placeholder=self.MASK,
+        )
+        input_items["token"] = secrets["token"]
 
         headers = {
             "X-Version": "3",
